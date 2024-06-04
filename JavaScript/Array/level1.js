a92f6e73d5526bdc87610a925f786778bac0a9c7
Ankit Shrestha <ankitshrestha008@gmail.com>
2024-06-04 17:13:08 +0545

    DivisibleBy3

---
+++ JavaScript/Array/level1.js
@@ -0,0 +1,23 @@
+//Q1 find list of numbers divisible by 3
+
+const arr = [3,5,1,2,9,12]
+//write your code here
+console.log(arr)
+
+//expected output---> [3,9,12]
+// hint: %3 === 0
+// filter
+//syntax:
+// arr.filter((item,id)=>{
+//     //
+// })
+
+
+//----------------
+const arr = [3,5,1,2,9,12]
+arr.filter((item)=>{
+  if (item%3 === 0){
+    return item
+  }
+})
+
