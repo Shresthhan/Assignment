f6cdcfcdba70411eb62453b3a50ba172d2abc2e8
Ankit Shrestha <ankitshrestha008@gmail.com>
2024-06-06 17:13:07 +0545

    Complete

--- JavaScript/Object/Level1.js
+++ JavaScript/Object/Level1.js
@@ -3,3 +3,67 @@
 // 'Civic'
 const car1 = { make: 'Honda', model: 'Civic', year: 2020 };
 console.log(car1.model)
+
+// Q2: Add a new property 'color' with the value 'red' to the 'shirt' object.
+// Expected output:
+// { size: 'large', material: 'cotton', color: 'red' }
+const shirt2 = { size: 'large', material: 'cotton' };
+shirt2.color="red";
+console.log(shirt2)
+
+// Q3: Calculate the total price by multiplying the 'quantity' and 'price' properties of the 'product' object.
+// Expected output:
+// 400
+const product3 = { name: 'Laptop', quantity: 2, price: 200 };;
+let a = product3.quantity * product3.price;
+console.log(a);
+
+// Q4: Update the value of the 'age' property in the 'person' object to 35.
+// Expected output:
+// { name: 'Alice', age: 35, city: 'New York' }
+const person4 = { name: 'Alice', age: 30, city: 'New York' };
+person4.age = 35;
+console.log(person4)
+
+
+// Q5: Remove the 'price' property from the 'book' object.
+// Expected output:
+// { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', year: 1925 }
+const book5 = { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', year: 1925, price: 15 };
+delete book5.price;
+console.log(book5);
+
+// Q6: Retrieve the value of the 'department' property from the 'employee' object.
+// Expected output:
+// 'HR'
+const employee6 = { name: 'John', age: 28, department: 'HR', salary: 60000 };
+let a = employee6.department;
+console.log(a);
+
+// Q7: Add a new property 'size' with the value 'large' to the 'box' object.
+// Expected output:
+// { type: 'box', dimensions: { length: 10, width: 8, height: 6 }, size: 'large' }
+const box7 = { type: 'box', dimensions: { length: 10, width: 8, height: 6 } };
+box7.dimensions.size="large";
+console.log(box7);
+
+// Q8: Concatenate the 'firstName' and 'lastName' properties of the 'person' object with a space in between.
+// Expected output:
+// 'John Doe'
+const person8 = { firstName: 'John', lastName: 'Doe', age: 25 };
+let a = person8.firstName + ' ' + person8.lastName;
+console.log(a);
+
+
+// Q10: Check if the 'price' property in the 'product' object is less than 100.
+// Expected output:
+// false
+const product10 = { name: 'Phone', price: 150 };
+let value = product10.price;
+function isLessThan(value){
+  if ( value < 100){
+   return true;
+}else
+  return false;
+}
+isLessThan();
