9ea77594d990b1b535568ba9e8649590a3b36964
Ankit Shrestha <ankitshrestha008@gmail.com>
2024-06-06 16:51:52 +0545

    output

---
+++ JavaScript/Object/Level1.js
@@ -0,0 +1,5 @@
+// Q1: Retrieve the value of the 'model' property from the 'car' object.
+// Expected output:
+// 'Civic'
+const car1 = { make: 'Honda', model: 'Civic', year: 2020 };
+console.log(car1.model)
